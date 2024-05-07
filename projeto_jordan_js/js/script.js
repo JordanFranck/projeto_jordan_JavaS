4b6bc22204a23501fc646ab09d8de28616e850b1
JordanFranck <161588431+JordanFranck@users.noreply.github.com>
2024-05-07 08:44:43 -0300

    Update script.js

--- projeto_jordan_js/js/script.js
+++ projeto_jordan_js/js/script.js
@@ -2,14 +2,14 @@ var meuFormulario = document.getElementById("formulario");
 meuFormulario.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  //Chama a função somar
-  somar();
+  //Chama a função percentual
+  porcentagem();
 
 });
 
 
-//Função somar
-function somar() {
+//Função percentual
+function porcentagem() {
 
   //Captura o que está dentro do primeiro input no formulário
   //Number - Transforma o texto capturado em Numero
@@ -19,14 +19,13 @@ function somar() {
   //Number - Transforma o texto capturado em Numero
   let numero_ascesso_internet = Number(document.getElementById("numero_ascesso_internet").value);
 
-  //Realiza a soma do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
-  //variável "segundo_numero_inteiro"
-  //E a variável "resultado_soma" recebe o valor da soma
+  //Realiza o calculo do percentual do número de alunos com ascesso a internet
+  //E a variável "porcentagem_ascesso_internet" recebe o valor da porcentagem
   let porcentagem_ascesso_internet = numero_ascesso_internet / numero_alunos * 100;
 
   porcentagem_ascesso_internet = porcentagem_ascesso_internet + "%"
 
-  //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_soma"
+  //Aqui vai escrever no input "porcentagem_ascesso_internet" o valor que está armazenado na variável "porcentagem_ascesso_internet"
   document.getElementById("porcentagem_ascesso_internet").value = porcentagem_ascesso_internet;
 
 
